084bf5ca9a88511b279a42818da9d38626383c7f
hakuunabatata <emachadomartins1999@gmail.com>
2023-07-05 21:17:14 -0300

    create project

---
+++ bin/first_steps.dart
@@ -0,0 +1,5 @@
+import 'package:first_steps/first_steps.dart' as first_steps;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_steps.calculate()}!');
+}
---
+++ test/first_steps_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_steps/first_steps.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
