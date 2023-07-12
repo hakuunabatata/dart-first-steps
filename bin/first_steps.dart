302867e355b9acdb6462e0c188cec8b68eb309c8
hakuunabatata <emachadomartins1999@gmail.com>
2023-07-12 19:56:12 -0300

    tests with classes

--- bin/first_steps.dart
+++ bin/first_steps.dart
@@ -1,3 +1,16 @@
 void main(List<String> arguments) {
   print('Hello Mundo');
+  Alpha alpha = Alpha(2);
+
+  int num = alpha.calculate(3);
+
+  print(num);
+}
+
+class Alpha {
+  final int a;
+
+  Alpha(this.a);
+
+  int calculate(int b) => a + b;
 }
