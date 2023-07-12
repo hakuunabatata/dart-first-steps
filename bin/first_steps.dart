187884cc886b919d59baeba68e8ca7994d343850
hakuunabatata <emachadomartins1999@gmail.com>
2023-07-12 19:50:20 -0300

    fix tests

--- bin/first_steps.dart
+++ bin/first_steps.dart
@@ -1,21 +1,3 @@
-import 'package:first_steps/first_steps.dart' show calculate, test;
-
 void main(List<String> arguments) {
-  const a = 1;
-  double b = 1;
-
-  const name2 = a >= 1 ? 12 : 35;
-
-  x(int a) => test(a.toDouble(), 10);
-
-  do {
-    b -= 1;
-  } while (b > 0);
-
-  const Map<String, double> obj = {"c": 1, 'd': 8};
-
-  print(obj);
-
-  print('Hello world: ${calculate(a, b)} ${x(name2)}!');
-  print(DateTime.now());
+  print('Hello Mundo');
 }
--- test/first_steps_test.dart
+++ test/first_steps_test.dart
@@ -1,8 +1,3 @@
-import 'package:first_steps/first_steps.dart';
-import 'package:test/test.dart';
-
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  print('Hello World');
 }
