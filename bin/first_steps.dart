f7eecb6ab5709f6d5a49ff51eb781dd7a7f4a9b5
hakuunabatata <emachadomartins1999@gmail.com>
2023-07-12 19:42:07 -0300

    do another lang tests

--- bin/first_steps.dart
+++ bin/first_steps.dart
@@ -1,8 +1,21 @@
-import 'package:first_steps/first_steps.dart' as first_steps;
+import 'package:first_steps/first_steps.dart' show calculate, test;
 
 void main(List<String> arguments) {
-  int a = 1;
-  double b = 1.5;
+  const a = 1;
+  double b = 1;
 
-  print('Hello world: ${first_steps.calculate(a, b)}!');
+  const name2 = a >= 1 ? 12 : 35;
+
+  x(int a) => test(a.toDouble(), 10);
+
+  do {
+    b -= 1;
+  } while (b > 0);
+
+  const Map<String, double> obj = {"c": 1, 'd': 8};
+
+  print(obj);
+
+  print('Hello world: ${calculate(a, b)} ${x(name2)}!');
+  print(DateTime.now());
 }
--- lib/first_steps.dart
+++ lib/first_steps.dart
@@ -1,3 +1,5 @@
 double calculate(int a, double b) {
   return 6 * 7 + a - b;
 }
+
+double test(double test, int multiple) => test * multiple;
